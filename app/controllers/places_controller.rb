613c0942026979d40cc1bfa63c7256399207501c
Ben Resnick <ben.resnick@kellogg.northwestern.edu>
2025-02-21 19:58:43 +0000

    orginal set up of places

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,32 @@
+class PlacesController < ApplicationController
+
+
+  def index
+    @places = Place.all
+    
+  end
+  
+  def new
+
+  end
+
+  def create
+    
+    @place = Place.new
+
+    # assign user-entered form data to Post's columns
+    @place["name"] = params["name"]
+
+
+    # save Post row
+    @place.save
+
+    # redirect user
+    redirect_to "/places"
+  end
+
+
+
+
+
+end
